c811e4ea9492c1c2d9aa78c19d48339ec01cff1a
RogithRio <rogithkivi123@gmail.com>
2024-05-05 15:02:00 +0530

    added video

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 onload = () =>{
     document.body.classList.remove("container");
 };
+
+// Function to redirect to YouTube video
+function redirectToYouTubeVideo() {
+    // Replace 'VIDEO_ID' with the ID of the YouTube video you want to redirect to
+    var videoId = 'VIDEO_ID';
+    var redirectUrl = 'https://www.youtube.com/watch?v=qhg0Zaw3e8M';
+    window.location.href = redirectUrl;
+}
+
+// Set the time after which you want to redirect (in milliseconds)
+var redirectTime = 20000; // 5000 milliseconds = 5 seconds
+
+// Wait for the specified time and then redirect
+setTimeout(redirectToYouTubeVideo, redirectTime);
